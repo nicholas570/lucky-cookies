952a957214a41447cfd7b5d80064d686f416f488
nicholas570 <nicolas.mercier570@gmail.com>
2020-09-01 14:46:24 +0200

    adding axios and creating a fetching data function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 function App() {
-  return <div className="App">Hello Lucky-Cookies</div>;
+  const [main, setMain] = useState([]);
+
+  const fetchData = async () => {
+    await axios
+      .get('/data.json')
+      .then((res) => {
+        setMain(res.data.main);
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div className="App">{main.brand}</div>;
 }
 
 export default App;

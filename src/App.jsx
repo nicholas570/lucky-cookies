a29720c9e271343d1b37bc4a2da4d2cc87c3a7f2
nicholas570 <nicolas.mercier570@gmail.com>
2020-09-01 20:06:40 +0200

    replacing assests in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import './App.css';
 
 function App() {
   const [main, setMain] = useState([]);
+  const [content, setContent] = useState([]);
+  const [shop, setShop] = useState([]);
+  const [goods, setGoods] = useState([]);
 
   const fetchData = async () => {
     await axios
       .get('/data.json')
       .then((res) => {
         setMain(res.data.main);
+        setContent(res.data.content);
+        setShop(res.data.shop);
+        setGoods(res.data.goods);
       })
       .catch((err) => alert(err.message));
   };
@@ -19,7 +25,15 @@ function App() {
     fetchData();
   }, []);
 
-  return <div className="App">{main.brand}</div>;
+  return (
+    <div
+      className="App"
+      main={main}
+      content={content}
+      shop={shop}
+      goods={goods}
+    />
+  );
 }
 
 export default App;

5f60426db08fb9201b69d586011a28f3112e7ef1
nicholas570 <nicolas.mercier570@gmail.com>
2020-09-01 12:57:22 +0200

    adding all images

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello Lucky-Cookies</div>;
+  return <div className="App">Hello Lucky-Cookies</div>;
 }
 
 export default App;

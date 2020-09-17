e2009e4977896109bdfa24d6dbf476be6461b7f0
nicholas570 <nicolas.mercier570@gmail.com>
2020-09-17 11:12:47 +0200

    adding modal component

---
+++ src/components/contacts/Modal.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import { Button, Modal } from 'react-bootstrap';
+
+function ModalComponent() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button
+        variant="light"
+        className="font-weight-bold btn btn-outline-dark"
+        onClick={handleShow}
+      >
+        CONTACT
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, youre reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalComponent;

5bfbf6ccf5ca09a288a412148aa42706ffbe1054
nicholas570 <nicolas.mercier570@gmail.com>
2020-09-17 20:34:12 +0200

    handling form submition with nodemailer and express

--- src/components/contacts/ContactsForm.jsx
+++ src/components/contacts/ContactsForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 import { Col, Form, Button } from 'react-bootstrap';
 
@@ -11,12 +12,22 @@ function ContactsForm() {
     subject: '',
     message: '',
   });
+  const [result, setResult] = useState(null);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const form = e.currentTarget;
+    if (form.checkValidity()) {
+      axios
+        .post('/send', { ...state })
+        .then((res) => {
+          setResult(res.data);
+          setState({ name: '', email: '', subject: '', message: '' });
+        })
+        .catch(() => {
+          setResult({ success: false, message: 'Something went wrong' });
+        });
     }
 
     setValidated(true);
@@ -32,86 +43,89 @@ function ContactsForm() {
   };
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Form.Row>
-        <Form.Group as={Col} md="6" controlId="firstName">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            type="text"
-            name="firstName"
-            placeholder="First name"
-            value={state.firstName}
-            onChange={handleChange}
-            required
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="6" controlId="lastName">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Last name"
-            name="lastName"
-            value={state.lastName}
-            onChange={handleChange}
-            required
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-      <Form.Row>
-        <Form.Group as={Col} md="12" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Email"
-            name="email"
-            value={state.email}
-            onChange={handleChange}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid email.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="12" controlId="subject">
-          <Form.Label>Subject</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Subject"
-            name="subject"
-            value={state.subject}
-            onChange={handleChange}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid subject.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="12" controlId="message">
-          <Form.Label>message</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={5}
-            placeholder="Message"
-            name="message"
-            value={state.message}
-            onChange={handleChange}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid message.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Form.Row>
-      <Button
-        type="submit"
-        variant="light"
-        className="font-weight-bold btn btn-outline-dark"
-      >
-        Submit
-      </Button>
-    </Form>
+    <>
+      {result && <p>{result.message}</p>}
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Row>
+          <Form.Group as={Col} md="6" controlId="firstName">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              type="text"
+              name="firstName"
+              placeholder="First name"
+              value={state.firstName}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="lastName">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Last name"
+              name="lastName"
+              value={state.lastName}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} md="12" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="12" controlId="subject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Subject"
+              name="subject"
+              value={state.subject}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid subject.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="12" controlId="message">
+            <Form.Label>message</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Message"
+              name="message"
+              value={state.message}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid message.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form.Row>
+        <Button
+          type="submit"
+          variant="light"
+          className="font-weight-bold btn btn-outline-dark"
+        >
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 }
 

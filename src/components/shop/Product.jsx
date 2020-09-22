a20eac9cf876ea285b151ec2b18d38f4d2ba5ec2
nicholas570 <nicolas.mercier570@gmail.com>
2020-09-22 16:49:40 +0200

    resizing all cookies image

--- src/components/shop/Product.jsx
+++ src/components/shop/Product.jsx
@@ -8,7 +8,11 @@ function Product({ good }) {
   return (
     <Col md={4} className="p-0">
       <Card style={{ width: '100' }} className="border border-0 px-2">
-        <Card.Img variant="top" src={`/assets/images/shop/${good.pic}`} />
+        <Card.Img
+          variant="top"
+          className="d-block w-100 h-75"
+          src={`/assets/images/shop/${good.pic}`}
+        />
         <Card.Body>
           <Card.Title className="text-left">{good.name}</Card.Title>
           <Card.Text className="text-left">{good.price}</Card.Text>
